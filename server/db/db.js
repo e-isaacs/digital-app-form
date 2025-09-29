eed59a70ade4d7b4b12e2478ef9dabf8cbdce5c7
Ethan Isaacs <ethanisaacs@inhalecapital.co.uk>
2025-09-29 11:54:25 +0100

    fix

--- server/db/db.js
+++ server/db/db.js
@@ -1,12 +1,9 @@
+// server/db/db.js
 import pkg from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
-
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL || "postgres://localhost:5432/digital_app_form",
   ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
